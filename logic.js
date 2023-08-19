d5db0434ea255714bc1c9bf38f28da451e9903ea
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2023-08-19 09:04:32 +0530

    Add files via upload
    My first project using javaScript

---
+++ logic.js
@@ -0,0 +1,45 @@
+/* document.querySelector("h2").style.color = "blue";
+document.querySelector("body").style.backgroundColor = "black";
+document.querySelector("h1").style.backgroundColor = "blue";
+document.querySelector("h2").style.backgroundColor = "red";
+var a = document.querySelector("h1");
+a.addEventListener("click", function () {
+    document.querySelector("body").style.backgroundColor = "blue";
+})
+
+var a = document.querySelector("h2");
+a.addEventListener("click", function () {
+    document.querySelector("body").style.backgroundColor = "red";
+})
+*/
+/*var a = document.querySelector("body");
+a.addEventListener("click", function () {
+    document.querySelector("body").style.backgroundColor = "blue";
+})*/
+var a = document.querySelector(".btn1");
+a.addEventListener("click", function () {
+    document.querySelector(".circle").style.backgroundColor = "#FF1493";
+    
+})
+
+var a = document.querySelector(".btn2");
+a.addEventListener("click", function () {
+    document.querySelector(".circle").style.backgroundColor = "#22fb42";
+})
+var a = document.querySelector(".btn3");
+a.addEventListener("click", function () {
+    document.querySelector(".circle").style.backgroundColor = " #FFFF00";
+})
+var a = document.querySelector(".btn4");
+a.addEventListener("click", function () {
+    document.querySelector(".circle").style.backgroundColor = "red";
+})
+var a = document.querySelector(".btn5");
+a.addEventListener("click", function () {
+    document.querySelector(".circle").style.backgroundColor = " #EE82EE";
+})
+var a = document.querySelector(".circle");
+a.addEventListener("click", function () {
+    document.querySelector(".circle").style.backgroundColor = "black";
+})
+
